da4c6433c57664dc799c8448cdeb8eff7c58fb76
casiimir <cp.ciancimino@gmail.com>
2023-06-27 11:27:27 +0200

    feat: add product section in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 // import cocktailLogo from "./assets/cocktail.svg";
+import ProductSection from "./components/productSection/ProductSection";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [productSection, setProductSection] = useState("margarita");
+
+  return (
+    <div className="App">
+      <ProductSection productSection={productSection} />
+    </div>
+  );
 }
 
 export default App;
